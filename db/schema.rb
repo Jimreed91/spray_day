863c93e44855e10b35aa23d49a8703aa76abd8cc
James Reed <jamesreed.3050@gmail.com>
2022-07-03 22:08:41 +0900

    testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_15_141809) do
+ActiveRecord::Schema.define(version: 2022_06_15_122448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "crops", force: :cascade do |t|
-    t.string "name"
-    t.integer "year"
-    t.decimal "hectares"
-    t.decimal "row_spacing"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
