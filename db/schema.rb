21ef72a746ee937c2402e15ddb6384df0b4113f8
James Reed <jamesreed.3050@gmail.com>
2022-07-03 23:07:35 +0900

    one to one user to farm

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_15_122448) do
+ActiveRecord::Schema.define(version: 2022_07_03_133113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "crops", force: :cascade do |t|
+    t.string "name"
+    t.integer "year"
+    t.decimal "hectares"
+    t.decimal "row_spacing"
+    t.bigint "farm_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["farm_id"], name: "index_crops_on_farm_id"
+  end
+
+  create_table "farms", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_farms_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +45,6 @@ ActiveRecord::Schema.define(version: 2022_06_15_122448) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "crops", "farms"
+  add_foreign_key "farms", "users"
 end
