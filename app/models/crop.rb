9aaf8de2cf72acb61719c9fa3a2f8e88ef60762f
James Reed <jamesreed.3050@gmail.com>
2022-07-03 23:25:44 +0900

    One to one working

--- app/models/crop.rb
+++ app/models/crop.rb
@@ -1,3 +1,4 @@
 class Crop < ApplicationRecord
   belongs_to :farm
+  has_one :user, through: :farm
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :farm
+  has_many :crops, through: :farm
 end
