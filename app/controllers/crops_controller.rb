f9e0da55712686984d42f1b4ee150859c2b00cdb
James Reed <jamesreed.3050@gmail.com>
2022-07-05 00:03:58 +0900

    Setting up controller

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -1,19 +1,33 @@
 class CropsController < ApplicationController
   def index
+    @crops = Crop.all
   end
 
   def show
+    @crop = Crop.find(params[:id])
   end
 
   def new
+    @crop = Crop.new
   end
 
   def create
+    @crop = Crop.new(crop_params)
+    if @crop.save
+      redirect_to crop_path(@crop)
+    else
+      render new_crop_path
+    end
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+  def crop_params
+    params.require(:crop).permit(name:, :year, :hectares, :row_spacing, :farm)
   end
 end
