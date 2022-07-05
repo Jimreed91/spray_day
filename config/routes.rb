06488e34cfe52da3a8f051ce7dbba36236a654b7
James Reed <jamesreed.3050@gmail.com>
2022-07-05 12:29:26 +0900

    Farm initial form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'farm/new'
-  get 'farm/create'
-  get 'farm/show'
-  get 'farm/index'
-  # get 'pages/main'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get '/pages', to: 'pages/#main'
@@ -13,5 +8,12 @@ Rails.application.routes.draw do
   get 'crops/new', to: 'crops#new'
   get 'crops/:id', to: 'crops#show'
   post '/crops', to: 'crops#create'
+
+
+  get 'farms/new', to: 'farms#new'
+  get 'farms/:id', to: 'farms#show', as: :farm
+  post 'farms', to: 'farms#create'
+  get 'farms/index', to: 'farms#index'
+
 
 end
