fe35dbe6724fc0d987724f20d615e79256282c74
James Reed <jamesreed.3050@gmail.com>
2022-07-14 16:55:25 +0900

    push

---
+++ spec/controllers/crops_controller_spec.rb
@@ -0,0 +1,11 @@
+# require 'rails_helper'
+
+# RSpec.describe CropsController, '#create' do
+
+#   context 'when the crop details are invalid' do
+#     it 're-renders the form' do
+#       post :create, crop: build(:crop, name: nil)
+#       expect(response).to render_template :new
+#     end
+#   end
+# end
