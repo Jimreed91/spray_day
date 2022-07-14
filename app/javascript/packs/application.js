3c70b8fc9858cb0f25c3b882f6831926c0268437
James Reed <jamesreed.3050@gmail.com>
2022-07-14 18:23:46 +0900

    application js importing bootstrapjs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ import 'channels'
 import "../../assets/stylesheets/application.scss"
 // import 'bootstrap'
 // Bootstrap linked from js folder /..
-// import '../js/bootstrap_js_files.js'
+import '../js/bootstrap_js_files.js'
 
 // Import the specific modules you may need (Modal, Alert, etc)
 // import { Tooltip, Popover } from 'bootstrap'
