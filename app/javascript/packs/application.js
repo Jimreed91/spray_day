9e540a8b7afaa7af1c05931cf7d60a682419ced5
James Reed <jamesreed.3050@gmail.com>
2022-07-14 19:01:53 +0900

    saving working state

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@ import Rails from '@rails/ujs'
 import Turbolinks from 'turbolinks'
 import * as ActiveStorage from '@rails/activestorage'
 import 'channels'
-import "../../assets/stylesheets/application.scss"
+// import "../../assets/stylesheets/application.scss"
 // import 'bootstrap'
 // Bootstrap linked from js folder /..
 // import '../js/bootstrap_js_files.js'
@@ -18,20 +18,3 @@ require('@popperjs/core')
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-// // The stylesheet location we created earlier
-// require('../stylesheets/application.scss')
-
-// If you're using Turbolinks. Otherwise simply use: jQuery(function () {
-// document.addEventListener('turbolinks:load', () => {
-//   // Both of these are from the Bootstrap 5 docs
-//   const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-//   const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-//     return new Tooltip(tooltipTriggerEl)
-//   })
-
-//   const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-//   const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-//     return new Popover(popoverTriggerEl)
-//   })
-// })
