aa665e9f7b73cd6634d81ce318ed81c2591082d0
James Reed <jamesreed.3050@gmail.com>
2022-07-14 18:26:59 +0900

    with bs imported in appjs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,12 +9,12 @@ import 'channels'
 import "../../assets/stylesheets/application.scss"
 // import 'bootstrap'
 // Bootstrap linked from js folder /..
-import '../js/bootstrap_js_files.js'
+// import '../js/bootstrap_js_files.js'
 
 // Import the specific modules you may need (Modal, Alert, etc)
 // import { Tooltip, Popover } from 'bootstrap'
 require('@popperjs/core')
-
+require('bootstrap')
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
