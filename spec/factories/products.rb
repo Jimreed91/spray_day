d59961d11f3e768c667fcca40f63bee7b4738144
James Reed <jamesreed.3050@gmail.com>
2022-07-15 16:34:43 +0900

    FactoryBot setup for product

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :product do
-    name { "MyString" }
-    rate_per_ha { "9.99" }
-    liquid { false }
-    farm { nil }
+    name { "#{Faker::Verb.ing_form} spray" }
+    rate_per_ha { rand(0.5...10) }
+    liquid { rand(0...10) > 5 }
+    farm { FactoryBot.create(:farm) }
   end
 end
