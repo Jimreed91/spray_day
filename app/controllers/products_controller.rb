2692386eb11dd0b7a71cede34becf1b1cb359e64
James Reed <jamesreed.3050@gmail.com>
2022-07-15 15:40:50 +0900

    added routing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,33 @@
 class ProductsController < ApplicationController
-  def create
+  def new
+    @product = Product.new(product_params)
   end
 
-  def new
+  def create
+
+
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def index
+    @products = Products.all
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  def update
+  end
+
+  private
+
+  def product_params()
+    params.require(:product).permit(:name, :rate_per_ha, :liquid)
   end
 end
