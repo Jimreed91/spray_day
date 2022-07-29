bba427cc0ce0d9ea56dcd7930bb70d1f2268e567
James Reed <jamesreed.3050@gmail.com>
2022-07-29 23:43:01 +0900

    adding some uniqueness validations

--- app/models/program_crop.rb
+++ app/models/program_crop.rb
@@ -1,4 +1,6 @@
 class ProgramCrop < ApplicationRecord
   belongs_to :crop
   belongs_to :spray_program
+
+  validates :spray_program, uniqueness: {scope: :crop}
 end
--- app/models/program_sprayer.rb
+++ app/models/program_sprayer.rb
@@ -1,4 +1,6 @@
 class ProgramSprayer < ApplicationRecord
   belongs_to :sprayer
   belongs_to :spray_program
+
+  validates :spray_program, uniqueness: {scope: :sprayer}
 end
