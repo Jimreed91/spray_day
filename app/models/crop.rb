9d634ee865c9f6b0213612b0a670cf2868b52185
James Reed <jamesreed.3050@gmail.com>
2022-08-03 16:44:37 +0900

    adjusting crop - sp to  depentdant: :nullify

--- app/models/crop.rb
+++ app/models/crop.rb
@@ -3,7 +3,7 @@
 class Crop < ApplicationRecord
   belongs_to :farm
   has_one :user, through: :farm, dependent: :destroy
-  has_many :program_crops, dependent: :nil
+  has_many :program_crops, dependent: :nullify
   has_many :spray_programs, through: :program_crops
 
   validates :name, presence: true
