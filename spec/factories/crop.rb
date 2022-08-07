28a66e9d6190b2be58e75868a07a02c8053dec38
James Reed <jamesreed.3050@gmail.com>
2022-08-07 15:56:54 +0900

    Passing

--- spec/factories/crop.rb
+++ spec/factories/crop.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     name { "#{Faker::Verb.ing_form} apple" }
     year { rand(2001...2022) }
     hectares { rand(1.2...3.5) }
-    row_spacing { rand(1.7...2.1) }
+    row_spacing { 3.25 }
     farm { FactoryBot.create(:farm) }
   end
 end
--- spec/models/crop_spec.rb
+++ spec/models/crop_spec.rb
@@ -1,25 +1,29 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-
 RSpec.describe Crop, type: :model do
+  it 'to be valid with valid attributes' do
+    crop = build(:crop)
+    expect(crop).to be_valid
+  end
+
   it 'is not valid without a name' do
-    crop = create(:crop, name: nil)
+    crop = build(:crop, name: nil)
     expect(crop).not_to be_valid
   end
 
   it 'is not valid without an area value' do
-    crop = create(:crop, hectares: nil)
+    crop = build(:crop, hectares: nil)
     expect(crop).not_to be_valid
   end
 
   it 'is not valid without a row_spacing' do
-    crop = create(:crop, row_spacing: nil)
+    crop = build(:crop, row_spacing: nil)
     expect(crop).not_to be_valid
   end
 
   it 'is not valid without a farm' do
-    crop = create(:crop, farm: nil)
+    crop = build(:crop, farm: nil)
     expect(crop).not_to be_valid
   end
 end
