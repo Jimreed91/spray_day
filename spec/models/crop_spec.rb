8e2f376e2135d3ba36a0ec18125df73725f7dce3
James Reed <jamesreed.3050@gmail.com>
2022-08-07 14:35:36 +0900

    Model tests for crop validations

--- spec/models/crop_spec.rb
+++ spec/models/crop_spec.rb
@@ -3,5 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe Crop, type: :model do
-  pending
+  it 'is not valid without a name' do
+    crop = create(:crop, name: nil)
+    expect(crop).not_to be_valid
+  end
+
+  it 'is not valid without an area value' do
+    crop = create(:crop, hectares: nil)
+    expect(crop).not_to be_valid
+  end
+
+  it 'is not valid without a row_spacing' do
+    crop = create(:crop, row_spacing: nil)
+    expect(crop).not_to be_valid
+  end
+
+  it 'is not valid without a farm' do
+    crop = create(:crop, farm: nil)
+    expect(crop).not_to be_valid
+  end
 end
