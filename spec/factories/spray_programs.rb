6a2322b6d3f00ad290b5e71365975a50b4e2594a
James Reed <jamesreed.3050@gmail.com>
2022-08-08 00:08:47 +0900

    rough factory-bot for spray program testing

--- spec/factories/spray_programs.rb
+++ spec/factories/spray_programs.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :spray_program do
     date { '2022-07-29 19:03:17' }
-    total_volume { '' }
-    tanks_count { 1 }
-    farm { nil }
+    # total_volume { rand(500...3800) }
+    # tanks_count { rand(1...4) }
+    farm { FactoryBot.create(:farm) }
+    trait :with_assoc do
+      program_sprayer { FactoryBot.create(:program_sprayer) }
+      program_crops { [FactoryBot.create(:program_crop)] }
+      program_doses { [FactoryBot.create(:program_dose)] }
+    end
   end
 end
