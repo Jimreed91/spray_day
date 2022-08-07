b67f1d884e6f3cc0c332123d640c000c989d587b
James Reed <jamesreed.3050@gmail.com>
2022-08-07 13:03:57 +0900

    total dose calculation

--- app/models/program_dose.rb
+++ app/models/program_dose.rb
@@ -3,4 +3,10 @@ class ProgramDose < ApplicationRecord
   belongs_to :spray_program
 
   validates :spray_program, uniqueness: { scope: :product }
+
+  def total_dose
+    (spray_program.total_area * product.rate_per_ha).to_f
+  end
+
+  def tank_dose; end
 end
