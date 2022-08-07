dbd096fe28428cc5367f095edfb37be68a5f3381
James Reed <jamesreed.3050@gmail.com>
2022-08-07 17:34:56 +0900

    Product model tests + validation

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :product do
     name { "#{Faker::Verb.ing_form} spray" }
     rate_per_ha { rand(0.5...10) }
-    liquid { rand(0...10) > 5 }
     farm { FactoryBot.create(:farm) }
+    liquid { [true, false].sample }
   end
 end
