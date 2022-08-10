5f6cd2a9f664de6aac427f23e4753dbe0918aeb1
James Reed <jamesreed.3050@gmail.com>
2022-08-11 00:06:47 +0900

    Major forms split of into partials

--- spec/features/crops_spec.rb
+++ spec/features/crops_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe 'crops', type: :feature do
     fill_in('crop_year', with: 2010)
     fill_in('crop_hectares', with: 2.2)
     fill_in('crop_row_spacing', with: 1.7)
-    select('Your Farm', from: 'crop_farm_id')
     click_on('commit')
     expect(page).to have_content('test apple')
   end
