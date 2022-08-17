c44128c5b787ab591d56a22639eecf8b778ece00
James Reed <jamesreed.3050@gmail.com>
2022-08-17 14:37:51 +0900

    main presence validations covered

--- app/models/program_sprayer.rb
+++ app/models/program_sprayer.rb
@@ -3,4 +3,5 @@ class ProgramSprayer < ApplicationRecord
   belongs_to :spray_program
 
   validates :spray_program, uniqueness: { scope: :sprayer }
+  validates :litres_per_min, :speed, presence: true
 end
