77d998d054f81dd96a79edb3703eba99c2686585
James Reed <jamesreed.3050@gmail.com>
2023-02-19 19:00:09 +0900

    sprayer route refactor

--- config/routes.rb
+++ config/routes.rb
@@ -6,18 +6,7 @@ Rails.application.routes.draw do
 
   resources :products
   resources :crops
-  get 'farms/new', to: 'farms#new'
-  get 'farms/:id', to: 'farms#show', as: :farm
-  post 'farms', to: 'farms#create'
-  get 'farms/', to: 'farms#index'
-
-  get 'sprayers/new', to: 'sprayers#new'
-  post 'sprayers', to: 'sprayers#create'
-  get 'sprayers/:id', to: 'sprayers#show', as: :sprayer
-  get 'sprayers', to: 'sprayers#index'
-  get 'sprayers/:id/edit', to: 'sprayers#edit'
-  patch 'sprayers/:id', to: 'sprayers#update'
-  delete 'sprayers/:id', to: 'sprayers#destroy'
+  resources :sprayers
 
   resources :spray_programs do
     resource :pdf_download, only: [:show]
